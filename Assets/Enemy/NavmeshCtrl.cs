04bc8e19552f05a4f65abcf415fcb25e218278b9
okabe783 <134473737+okabe783@users.noreply.github.com>
2024-07-27 02:03:11 +0900

    [add] 敵の追跡機能を作った

---
+++ Assets/Enemy/NavmeshCtrl.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+//Navmeshで動くCharacter
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavmeshCtrl : MonoBehaviour
+{
+    [SerializeField] private GameObject _player; //追跡対象
+    private State.EnemyMove _currentState = State.EnemyMove.Walking; //現在の状態
+    private State.EnemyMove _nextState = State.EnemyMove.Walking; //次の状態
+    
+    private NavMeshAgent _navMeshAgent;
+    
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        TargetPosition();
+        StopNavigation();
+    }
+
+    //追跡対象をSet
+    private void TargetPosition()
+    {
+        //NavmeshがActiveでnullではなければ
+        if (_navMeshAgent != null && _navMeshAgent.isActiveAndEnabled)
+        {
+            Debug.Log("aaa");
+            _nextState = State.EnemyMove.Chasing; //追跡を開始
+            _navMeshAgent.SetDestination(_player.transform.position);
+            _navMeshAgent.isStopped = false; //NavMeshをセット
+        }  
+    }
+
+    //ターゲットにたどり着いたとき
+    private void StopNavigation()
+    {
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance && !_navMeshAgent.pathPending)
+        {
+            _nextState = State.EnemyMove.Attacking; //攻撃を開始
+            _navMeshAgent.isStopped = true;
+            Debug.Log(_nextState);
+        }
+    }
+}
---
+++ Assets/State/State.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    public enum EnemyMove
+    {
+        Walking, //探索中
+        Chasing,　//追跡中
+        Attacking,　//攻撃
+        Died　//死亡
+    }
+}
